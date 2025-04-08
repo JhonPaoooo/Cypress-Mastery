e375ea0da82ddcb4bed6bd1e69f90dcba443484b
Pao <pao@test.com>
2025-04-08 10:34:06 +0800

    Create cypress.yml file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
+
+// npx cypress run --record --key ce7bfafb-3b32-4b2a-ad72-2cf8f48ab196
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "r3zdnt",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
