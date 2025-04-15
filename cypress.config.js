67591750f95695af6e6de2d0b7968a10365c8faa
Pao <pao@test.com>
2025-04-15 13:29:13 +0800

    update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,18 @@
 
 // npx cypress run --record --key ce7bfafb-3b32-4b2a-ad72-2cf8f48ab196
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "r3zdnt",
   experimentalStudio: true,
-  viewportHeight: 660,
-  viewportWidth: 1000,
+  //viewportHeight: 660,
+  //viewportWidth: 1000,
   defaultCommandTimeout: 1000,
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     charts: true,
-    reportPageTitle: 'custom-title',
+    reportPageTitle: 'Test Report',
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
@@ -20,6 +21,15 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        console.log('Running tests');
+        await beforeRunHook(details);
+      });
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
     },
   },
 });
